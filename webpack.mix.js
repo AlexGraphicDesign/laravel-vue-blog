9b2c4b12b4faed3d8d04fd4625ca11de7c024aeb
Webmaster MCA Patrimoine <webmaster@mcapatrimoine.com>
2021-10-04 14:01:23 +0200

    dev formation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +12,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js');
+mix.vue();
+mix.sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCSSUrls: false,
+        postCss:[tailwindcss('./tailwind.config.js')]
+});
